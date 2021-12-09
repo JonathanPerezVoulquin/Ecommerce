cc39b58c2b9466aadc9dd5c9314df480eafcb42b
Jonathan Perez <jonathanperezvoulquin@gmail.com>
2021-12-09 19:43:22 -0300

    adding environment variable to connect to db

--- shop/__init__.py
+++ shop/__init__.py
@@ -6,8 +6,8 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SECRET_KEY'] = 'asd sada ssassdd sasdewertfxcv'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('user_sqlite')#in this variable you have to put the data of your db	
+app.config['SECRET_KEY'] = os.environ.get('password_sqlite')   		 #in this variable you have to put the data of your db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'static/images')
